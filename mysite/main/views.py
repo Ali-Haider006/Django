85d1033ec95eddce84c5930617cd903a9c1e7488
Ali Haider <alihaidervector123@gmail.com>
2024-10-02 14:39:58 +0500

    added models and dbsqlite3

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>My First View </h1>")
+def index(response,id):
+    req = ToDoList.objects.get(id = id)
+    item = req.item_set.get()
+    return HttpResponse("<h1>%s%s</h1> " %(req,item.text))
+
+def name(response,name):
+    req = ToDoList.objects.get(name = name)
+    item = req.item_set.get()
+    return HttpResponse("<h1>%s</h1>" %(item.text))
+
+
+def homepage(response):
+    return HttpResponse("<h1> First Page</h1>")
